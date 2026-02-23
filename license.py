0f746c7e97b3f564dbcefc9415557b8202b80e2a
r1qt86666-cyber <r1qt86666@gmail.com>
2026-02-23 11:38:26 -0800

    Update license.py

--- license.py
+++ license.py
@@ -78,7 +78,5 @@ def check_license():
         }), 200
     
     return jsonify({"status": "fail", "reason": "invalid"}), 403
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=5000)
